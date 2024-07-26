23c05edbdfe2943c3d7658458ae32fb736d7b7e1
Dharmesh pal <dharmesh.pal@scaletech.xyz>
2024-07-26 12:42:22 +0530

    Added function for getCountryByCode and getStateOfCountry and GetAllCitiesOfState

--- src/constants/constant.ts
+++ src/constants/constant.ts
@@ -43,3 +43,75 @@ export const csvToJson = (csvString: string) => {
 
   return result;
 };
+
+let fetchedData: any = null; // Flag to store fetched data
+let isFetching = false;
+
+export async function fetchDataAndProcess() {
+  if (fetchedData !== null) {
+    // If data is already fetched, return it
+    return fetchedData;
+  }
+
+  if (isFetching) {
+    // If fetching is in progress, wait for it to complete
+    return new Promise((resolve, reject) => {
+      const interval = setInterval(() => {
+        if (fetchedData !== null) {
+          clearInterval(interval);
+          resolve(fetchedData);
+        } else if (!isFetching) {
+          clearInterval(interval);
+          reject(new Error("Fetching failed"));
+        }
+      }, 100);
+    });
+  }
+
+  isFetching = true;
+
+  try {
+    const url = "https://country-city-search.s3.eu-north-1.amazonaws.com/country-city-search.csv";
+    const response = await fetch(url, {
+      headers: {
+        "Content-Type": "text/csv",
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error(`Network response was not ok: ${response.statusText}`);
+    }
+
+    const csvData = await response.text();
+    const parsedData = csvToJson(csvData);
+    fetchedData = processJsonData(parsedData);
+    return fetchedData;
+  } catch (error) {
+    console.error("Error fetching or parsing JSON:", error);
+    throw error;
+  } finally {
+    isFetching = false;
+  }
+}
+function processJsonData(jsonData: any) {
+  // Process jsonData here if needed
+  return jsonData;
+}
+
+export const fetchDataFromJson = async () => {
+  try {
+    const url = "https://country-city-search.s3.eu-north-1.amazonaws.com/countries-states.json";
+    const response = await fetch(url);
+
+    // Check if the response status is not OK (200-299 range)
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error fetching or parsing JSON:", error);
+    throw error;
+  }
+};
--- src/types.ts
+++ src/types.ts
@@ -19,3 +19,34 @@ export type ICityObject = {
   timezone: string;
   time_zone_name: string;
 };
+
+export type IDynamicObject = {
+  [key: string]: any;
+};
+
+export type ICountriesObject = {
+  id: number;
+  name: string;
+  iso3: string;
+  iso2: string;
+  numeric_code: string;
+  phone_code: string;
+  capital: string;
+  currency: string;
+  currency_name: string;
+  currency_symbol: string;
+  tld: string;
+  native: string;
+  region: string;
+  region_id: string;
+  subregion: string;
+  subregion_id: string;
+  nationality: string;
+  timezones: IDynamicObject[];
+  translations: IDynamicObject[];
+  states: IDynamicObject[];
+  latitude: string;
+  longitude: string;
+  emoji: string;
+  emojiU: string;
+};
